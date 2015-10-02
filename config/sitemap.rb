3fa6f57c4420f020993264f188778eb04cf559c8
Dave Shaffer <dave.shaffer@gmail.com>
2015-10-01 21:51:49 -0400

    Adjust protocols and paths on sitemaps

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -1,7 +1,8 @@
 # Set the host name for URL creation
 SitemapGenerator::Sitemap.default_host = "http://www.atouchoftranquilityspa.com"
 SitemapGenerator::Sitemap.adapter = SitemapGenerator::WaveAdapter.new
-SitemapGenerator::Sitemap.sitemaps_host = "http://#{ENV['FOG_DIRECTORY']}.s3.amazonaws.com/"
+SitemapGenerator::Sitemap.sitemaps_host = "https://s3.amazonaws.com/#{ENV['FOG_DIRECTORY']}/"
+SitemapGenerator::Sitemap.public_path = 'tmp/'
 SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
