1560c2cba28862056a07d57b78a9cc60cb0cd359
Dave Shaffer <dave.shaffer@gmail.com>
2015-10-15 00:00:21 -0400

    Update errors controller and tests

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -5,19 +5,19 @@ class ErrorsController < ApplicationController
   end
 
   def not_found
-    render status: 404
+    render status: 404, locals: { message: 'The resource you are trying to reach cannot be found.' }
   end
 
   def internal_server_error
-    render status: 500
+    render status: 500, locals: { message: 'There was an error processing this request.' }
   end
 
   def forbidden
-    render status: 403
+    render status: 403, locals: { message: "Uh-oh, you don't have permission to see this resource!" }
   end
 
   def bad_request
-    render status: 422
+    render status: 422, locals: { message: 'The change you wanted was rejected.' }
   end
 
   def error
--- test/controllers/errors_controller_test.rb
+++ test/controllers/errors_controller_test.rb
@@ -1,19 +1,24 @@
 require 'test_helper'
 
 class ErrorsControllerTest < ActionController::TestCase
-  test "should get not_found" do
+  test 'should get not_found when browsed to directly' do
     get :not_found
-    assert_response :success
+    assert_response 404
   end
 
-  test "should get internal_server_error" do
+  test 'should get internal_server_error when browsed to directly' do
     get :internal_server_error
-    assert_response :success
+    assert_response 500
   end
 
-  test "should get forbidden" do
+  test 'should get forbidden when browsed to directly' do
     get :forbidden
-    assert_response :success
+    assert_response 403
+  end
+
+  test 'should get bad_request when browsed to directly' do
+    get :bad_request
+    assert_response 422
   end
 
 end
