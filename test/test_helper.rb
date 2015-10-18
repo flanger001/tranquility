5db1beb11a4f8998b83286369cb2cefd69a3d0c3
Dave Shaffer <dave.shaffer@gmail.com>
2015-10-18 10:46:46 -0400

    Annotate models, make test helpers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,12 +9,19 @@ Capybara::Webkit.configure do |config|
   config.allow_url("www.genbook.com")
 end
 
+module ControllerMethods
+  def sign_in(user)
+    if user.authenticate(user[:password])
+      session[:user_id] = user.id
+    end
+  end
+end
 
 class ActiveSupport::TestCase
   include FactoryGirl::Syntax::Methods
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
-
+  include ControllerMethods
   # Add more helper methods to be used by all tests here...
 end
 
