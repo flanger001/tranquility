71d52736ad6b1f084a97159325c932ef376daba6
Dave Shaffer <dave.shaffer@gmail.com>
2015-11-06 11:47:26 -0500

    Add modal helper, update staff page to use helper

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -5,4 +5,10 @@ class UserFlowsTestTest < ActionDispatch::IntegrationTest
     visit '/'
     assert page.has_content?('Click here to purchase a Gift Certificate!')
   end
+
+  test 'modals show up on staff page' do
+    visit '/about'
+    assert page.status_code == 200
+    assert page.has_selector?('.member .modal.fade')
+  end
 end
