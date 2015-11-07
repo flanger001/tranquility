025312b2df0074425c469c0feb447cab118135ff
Dave Shaffer <dave.shaffer@gmail.com>
2015-11-07 17:07:38 -0500

    Add CKEditor to admin edit pages

--- test/integration/user_flows_test.rb
+++ test/integration/user_flows_test.rb
@@ -11,4 +11,16 @@ class UserFlowsTestTest < ActionDispatch::IntegrationTest
     assert page.status_code == 200
     assert page.has_selector?('.member .modal.fade')
   end
+
+  test 'CKEditor only shows up on admin edit pages' do
+    post_via_redirect sessions_path, session: { email: 'admin@example.com', password: 'password' }
+    assert_response 200
+    assert_select 'script:match("src", ?)', '//cdn.ckeditor.com/4.5.4/basic/ckeditor.js', count: 0
+
+    get edit_admin_category_collection_path(category_collections(:bodywork))
+    assert_select 'script:match("src", ?)', '//cdn.ckeditor.com/4.5.4/basic/ckeditor.js', count: 1
+
+    get root_path
+    assert_select 'script:match("src", ?)', '//cdn.ckeditor.com/4.5.4/basic/ckeditor.js', count: 0
+  end
 end
