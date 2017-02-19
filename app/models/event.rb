962772cee0968dcce15e80f6e2b2b033bc68087c
Dave Shaffer <dave.shaffer@gmail.com>
2017-02-19 09:38:49 -0500

    Fix events breaking staff page

--- app/models/event.rb
+++ app/models/event.rb
@@ -17,6 +17,10 @@ class Event < ActiveRecord::Base
 
   validates :day, presence: true
 
+  before_save do
+    self.off = true unless start_time.present? && end_time.present?
+  end
+
   def span
     off ? 'Off' : "#{start_time.strftime('%l:%M %P')} - #{end_time.strftime('%l:%M %P')}"
   end
