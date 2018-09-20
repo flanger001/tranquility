9bb06571ae5419c168b5b0c581dfdf2449044c60
David Shaffer <dave.shaffer@gmail.com>
2018-09-20 09:31:27 -0400

    Format app js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,26 +1,25 @@
 //= require jquery
-//= require jquery_ujs
 //= require bootstrap-sprockets
 //= require turbolinks
 
-$(document).on( 'page:load page:restore ready', function(e) {
-  $('.flash').fadeOut(1000, function(e) {
-    $(this).remove();
-  });
+$(document).on('page:load page:restore ready', function (e) {
+    $('.flash').fadeOut(1000, function (e) {
+        $(this).remove();
+    });
 
-  // Mailchimp email signup form
-  $('form[name="mc-embedded-subscribe-form"]').on('submit', function(e) {
-    $('#mailchimp-signup').modal('hide');
-  });
+    // Mailchimp email signup form
+    $('form[name="mc-embedded-subscribe-form"]').on('submit', function (e) {
+        $('#mailchimp-signup').modal('hide');
+    });
 
-  $('form').on('click', '.add_fields', function(e) {
-    var time = new Date().getTime();
-    var regexp = new RegExp($(this).data('id'), 'g');
-    $(this).before($(this).data('fields').replace(regexp, time));
-    event.preventDefault();
-  });
+    $('form').on('click', '.add_fields', function (e) {
+        var time = new Date().getTime();
+        var regexp = new RegExp($(this).data('id'), 'g');
+        $(this).before($(this).data('fields').replace(regexp, time));
+        event.preventDefault();
+    });
 
-  $(document).on('change', '[name="change_password"]', function(e) {
-    $('.password-form').slideToggle()
-  })
+    $(document).on('change', '[name="change_password"]', function (e) {
+        $('.password-form').slideToggle()
+    })
 });
