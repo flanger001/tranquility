7cbffc7b30ed9031e7d637f894b705e9221bbb21
David Shaffer <dave.shaffer@gmail.com>
2019-01-23 18:48:20 -0500

    Don't auto-generate remote forms with form_with

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Tranquility
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.exceptions_app = self.routes
+
+    config.action_view.form_with_generates_remote_forms = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
