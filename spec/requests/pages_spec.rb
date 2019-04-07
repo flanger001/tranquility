ad5c704aae5aab9acf6435cd91b2d18ac6c74fef
David Shaffer <dave.shaffer@gmail.com>
2019-04-07 17:12:13 -0400

    Add request specs
    Signed-off-by: David Shaffer <dave.shaffer@gmail.com>

---
+++ spec/requests/pages_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe PagesController do
+  describe "#index" do
+    it "gets the root path" do
+      get root_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "#contact_us" do
+    it "works" do
+      get contact_us_path
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
