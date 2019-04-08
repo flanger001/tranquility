98e4bd2524073193465856cc6098ea6d1b0d1131
David Shaffer <dave.shaffer@gmail.com>
2019-04-07 20:24:02 -0400

    Extract Event::Span class
    Signed-off-by: David Shaffer <dave.shaffer@gmail.com>

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ActiveRecord::Base
-  DAYS = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+  DAYS = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday).freeze
 
   belongs_to :schedule, counter_cache: true, optional: false
 
@@ -10,6 +10,22 @@ class Event < ActiveRecord::Base
   end
 
   def span
-    off ? 'Off' : "#{start_time.strftime('%l:%M %P')} - #{end_time.strftime('%l:%M %P')}"
+    Span.new(self)
+  end
+
+  class Span
+    attr_reader :event
+
+    def initialize(event)
+      @event = event
+    end
+
+    def to_s
+      if event.off
+        "Off"
+      else
+        "#{event.start_time.strftime("%l:%M %P")} - #{event.end_time.strftime("%l:%M %P")}"
+      end
+    end
   end
 end
