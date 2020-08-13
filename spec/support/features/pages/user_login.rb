64fbc7db8e1d9459d4fbd2c594db59bf9794c2ff
David Shaffer <dave.shaffer@gmail.com>
2020-08-13 10:18:36 -0400

    Refactor UserLogin page object and user sessions spec

--- spec/support/features/pages/user_login.rb
+++ spec/support/features/pages/user_login.rb
@@ -1,12 +1,14 @@
 class UserLogin
   extend Capybara::DSL
 
-  def self.run(email = "admin@example.com", password = "password")
-    visit "/login"
-    within "#login" do
-      fill_in "Email", :with => email
-      fill_in "Password", :with => password
+  class << self
+    def call(user)
+      visit "/login"
+      within "#login" do
+        fill_in "Email", :with => user.email
+        fill_in "Password", :with => "password"
+      end
+      click_button "Log In"
     end
-    click_button "Log In"
   end
 end
--- spec/system/user_sessions_spec.rb
+++ spec/system/user_sessions_spec.rb
@@ -1,21 +1,44 @@
 require "rails_helper"
 
 RSpec.describe "User sessions" do
+  let(:user) { create(:user) }
+
   describe "logging in" do
-    it "works" do
-      @user = create(:user)
-      UserLogin.run(@user.email, @user.password)
+    context "given no user" do
+      it "works" do
+        UserLogin.call(user)
+
+        expect(page.current_path).to eq(root_path)
+        expect(page).to have_text(user.first_name)
+      end
+    end
+
+    context "given a logged in user" do
+      it "redirects to the home page" do
+        UserLogin.call(user)
+
+        visit("/login")
+        expect(page.current_path).to eq(root_path)
+      end
     end
   end
 
   describe "logging out" do
-    it "also works" do
-      @user = create(:user)
-      UserLogin.run(@user.email, @user.password)
+    context "given a logged in user" do
+      it "logs the user out" do
+        UserLogin.call(user)
 
-      visit "/logout"
-      expect(page.current_path).to eq("/")
-      expect(page.body).to_not have_selector(".profile")
+        visit("/logout")
+        expect(page.current_path).to eq(root_path)
+        expect(page).to_not have_text(user.first_name)
+      end
+    end
+
+    context "given no user" do
+      it "redirects to the home page" do
+        visit("/logout")
+        expect(page.current_path).to eq(root_path)
+      end
     end
   end
 end
