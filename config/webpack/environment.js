ecce3fc33831f7c416c8b90d7f572994177fc398
David Shaffer <dave.shaffer@gmail.com>
2020-08-14 03:58:28 -0400

    Add all required JS dependencies

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
+const webpack = require("webpack");
+const provide = new webpack.ProvidePlugin({
+    $: "jquery",
+    jquery: "jquery",
+    jQuery: "jquery"
+});
 
+environment.plugins.prepend("Provide", provide);
 module.exports = environment
